ffcd3f8eb14e54170cce4e17b49fc492b62464d3
d-brandie.lucano <d-brandie.lucano@AFSWWDTF5X4588.us.accenturefederal.com>
2024-11-12 21:48:05 -0600

    renamed jsp views and files as directed

--- mgl-webapp_start/src/main/java/com/organization/mvcproject/mgltask1/controller/MGLTask1Controller.java
+++ mgl-webapp_start/src/main/java/com/organization/mvcproject/mgltask1/controller/MGLTask1Controller.java
@@ -35,10 +35,10 @@ public class MGLTask1Controller {
 	public ModelAndView review() {
 	/**
 	 * TODO 1.0 Rename the jsp view, to "reviewCreatePage" because it matches the URL triggering a circular view path error.
-	 * update games.jsp as well. 
+	 * update games.jsp as well. --
 	 * SEE:  https://www.baeldung.com/spring-circular-view-path-error
 	 */
-		return new ModelAndView("review", "command", new Review());
+		return new ModelAndView("reviewCreatePage", "command", new Review());
 	}
 
 	@RequestMapping(value = "/addReview", method = RequestMethod.POST)
@@ -48,9 +48,9 @@ public class MGLTask1Controller {
 		}
 	/**
 	 * TODO 1.0 Rename the jsp view, to "reviewDetailPage" because what is the view the "result" of?
-	 * update games.jsp as well. 
+	 * update games.jsp as well. --
 	 */
-		return new ModelAndView("result", "submittedReview", review);
+		return new ModelAndView("reviewDetailPage", "submittedReview", review);
 	}
 
 	
@@ -58,7 +58,7 @@ public class MGLTask1Controller {
 	public ModelAndView game() {
 		/**
 		 * TODO 1.0 Rename the jsp view, to "gamesPage" because it matches the URL triggering a circular view path error.
-		 * update games.jsp as well. 
+		 * update games.jsp as well.  --
 		 * SEE:  https://www.baeldung.com/spring-circular-view-path-error
 		 */
 		return new ModelAndView("gamesPage", "command", new Game());
